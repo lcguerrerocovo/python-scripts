a62f1b9f1a06ad515aa50d588a9544660aa7d94d
Luis Guerrero <luis.guerrero@revmob.com>
2017-11-03 09:52:36 -0200

    adding ability to run commands and switch iterm profiles

--- generate-iterm-layout.py
+++ generate-iterm-layout.py
@@ -11,9 +11,11 @@ import os
 
 parser = argparse.ArgumentParser(description='Generate iterm layouts to start ssh sessions for ec2 instances')
 parser.add_argument('filename', nargs='?', default = None, help='file with ec2 instance info')
+parser.add_argument('command', nargs='?', default = "ls", help='command to run on ssh')
 
 args = parser.parse_args()
 filename = args.filename
+command = args.command
 
 with open(filename, 'r') as fr:
    key = fr.readline()
@@ -24,4 +26,6 @@ with open(filename, 'r') as fr:
       fw.write("    layout: tiled" + os.linesep)
       fw.write("    panes:" + os.linesep)
       for line in fr:
-        fw.write("      - ssh -o \"StrictHostKeyChecking no\" -i ~/Development/keys/" + key.rstrip() + ".pem ec2-user@" + line.rstrip() + os.linesep)
+        fw.write("      - it2prof ssh; ssh -o \"StrictHostKeyChecking no\" -i ~/Development/keys/" \
+            + key.rstrip() + ".pem ec2-user@" + line.rstrip() \
+            + (" -t \'%s; /bin/bash -i\'" % command)+ os.linesep)

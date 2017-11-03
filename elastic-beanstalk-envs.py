2063f38565dafccc2a4bbd248292b768a88bb9dd
Luis Guerrero <luis.guerrero@revmob.com>
2017-11-03 10:07:22 -0200

    more cool changes :rocket:

--- elastic-beanstalk-envs.py
+++ elastic-beanstalk-envs.py
@@ -20,11 +20,13 @@ parser = argparse.ArgumentParser(description='List current beanstalk environment
 parser.add_argument('app_name', nargs='?', default = None, help='Elastic Beanstalk Application Name')
 parser.add_argument('region', nargs='?', default = DEFAULT_REGION, help='AWS Region')
 parser.add_argument('--console', action='store_true')
+parser.add_argument('--ec2', action='store_true')
 
 args = parser.parse_args()
 app_name = args.app_name
 region = args.region
 console = args.console
+ec2_info = args.ec2
 
 eb_client = boto3.client('elasticbeanstalk', region_name=region)
 ec2_client = boto3.client('ec2', region_name=region)
@@ -38,7 +40,7 @@ for index, env in enumerate(envs['Environments'],start=1):
 # user input
 while True:
   try:
-    choice = int(raw_input("Please choose environment to open web consoles: ")) - 1
+    choice = int(raw_input("Please choose eb environment: ")) - 1
     if choice < 0 or choice >= index:
       print("Not a valid choice")
       continue
@@ -53,17 +55,19 @@ env_id = envs['Environments'][choice]['EnvironmentId']
 resources = eb_client.describe_environment_resources(EnvironmentName=environment)
 
 if console:
-    webbrowser.open(LB_URL % (region,resources['EnvironmentResources']['LoadBalancers'][0]['Name']))
-    webbrowser.open(AG_URL % (region,resources['EnvironmentResources']['AutoScalingGroups'][0]['Name']))
-    webbrowser.open(EC2_URL % (region,environment))
-    webbrowser.open(EB_URL % (region,env_id))
+  print("[INFO] opening web consoles")
+  webbrowser.open(LB_URL % (region,resources['EnvironmentResources']['LoadBalancers'][0]['Name']))
+  webbrowser.open(AG_URL % (region,resources['EnvironmentResources']['AutoScalingGroups'][0]['Name']))
+  webbrowser.open(EC2_URL % (region,environment))
+  webbrowser.open(EB_URL % (region,env_id))
 
-instance_ids = [instance['Id'] for instance in resources['EnvironmentResources']['Instances']]
-
-with open("%s/%s.beanstalk" % (expanduser("~"),environment),'w') as file:
-  for i,id in enumerate(instance_ids):
-    response = ec2_client.describe_instances(InstanceIds=[id])
-    instances = response['Reservations'][0]['Instances']
-    if i == 0: file.write(instances[0]['KeyName'] + os.linesep)
-    for data in instances:
-      file.write(data['PrivateIpAddress'] + os.linesep)
+if ec2_info:
+  print("[INFO] generating file with eb env ec2 information")
+  instance_ids = [instance['Id'] for instance in resources['EnvironmentResources']['Instances']]
+  with open("%s/%s.beanstalk" % (expanduser("~"),environment),'w') as file:
+    for i,id in enumerate(instance_ids):
+      response = ec2_client.describe_instances(InstanceIds=[id])
+      instances = response['Reservations'][0]['Instances']
+      if i == 0: file.write(instances[0]['KeyName'] + os.linesep)
+      for data in instances:
+        file.write(data['PrivateIpAddress'] + os.linesep)
